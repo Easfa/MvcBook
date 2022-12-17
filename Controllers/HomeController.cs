84a69626bfdfd54f3c37bb9aad6beff9616370f4
Edanur ŞENGÜR <edanurrsengur@gmail.com>
2022-12-18 00:12:37 +0300

    Rastgele 3 data listeleme işlemi ve testler yapldı

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MvcBook.Models;
+using MvcWorkspace.Data;
+using MvcWorkspace.Models;
+using System.Linq;
 
 namespace MvcBook.Controllers;
 
 public class HomeController : Controller
 {
+
     //start here
     private readonly ILogger<HomeController> _logger;
 
@@ -14,10 +18,26 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+
     public IActionResult Index()
     {
-        return View();
+        //Dummy Datalar
+        //Veritabanından veriler buraya gelicek ama oluşturulan liste ismi değişmeyecek
+        var books = new List<DenemeData>
+    {
+       new DenemeData{ Id=1, Adi="Deneme1", SayfaSayisi=25},
+       new DenemeData{ Id=2, Adi="Deneme2", SayfaSayisi=20},
+       new DenemeData{ Id=3, Adi="Deneme3", SayfaSayisi=30},
+       new DenemeData{ Id=4, Adi="Deneme4", SayfaSayisi=300}
+    };
+        //Rastgele 3 kitap listeleme İşlemleri
+        var r = new Random();
+        var ThreeRandomBookList = new List<DenemeData>();
+        ThreeRandomBookList = books.OrderBy(x => r.Next()).Take(3).ToList();
+        //Rastgele 3 kitap listeleme İşlemleri Bitiş 
+        return View(ThreeRandomBookList);
     }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
---
+++ Models/DenemeData.cs
@@ -0,0 +1,9 @@
+namespace MvcWorkspace.Models
+{
+    public class DenemeData
+    {
+       public int Id { get; set; }
+       public string Adi { get; set; }
+       public int SayfaSayisi { get; set; }
+    }
+}
