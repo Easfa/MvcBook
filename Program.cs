240f56dfa10ee0c49c80ec72e75e39d549a2bff7
Uluç Meriç <ulucmrce@gmail.com>
2022-12-18 22:27:48 +0300

    2 Nokta koydum "test"

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline...
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
