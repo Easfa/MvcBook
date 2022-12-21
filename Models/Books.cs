89c8997d43008133c8b2ec207af68a91f2a01106
AnimusVx <ulucmrce@gmail.com>
2022-12-21 11:28:12 +0300

    Model uptaded. ToString added and image added

--- Models/Books.cs
+++ Models/Books.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcBook.Models
@@ -7,10 +8,24 @@ namespace MvcBook.Models
     {
         [Key]
         [Required]
+        [DisplayName("BookID")]
         public virtual Users B_Id { get; set; }
         [Required]
+        [DisplayName("Book Name")]
         public string B_Name { get; set; }
+        [DisplayName("Author")]
         public string B_Author { get; set; }
+        [Display("Page Numbers")]
         public string B_Page { get; set; }
+        public string Image { get; set; }
+
+        public override string ToString()
+        {
+            return B_Id + " - " + B_Name + " - " + B_Author + " - " + B_Page;
+        }
+        public string ToString()
+        {
+
+        }
     }
 }
